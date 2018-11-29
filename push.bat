4f82b48ec393a973a280327d9fa1b14ac05b10c8
nagasitaramthigulla <nagasitaram.ram@gmail.com>
2018-11-29 20:47:37 +0530

    commit 20:47:37.21,Thu 11/29/2018

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 git init .
 git add .
 git commit -m  "commit %time%,%date%"
-git -rm SaanviDashboard/
+git rm  --cached SaanviDashboard
 git remote add -m origin qnaforum https://github.com/nagasitaramthigulla/sanvi-bot-django.git
 git push -u origin master
