ffc136f6533c5ac3e89618e9ef12b3ac742a60b0
nagasitaramthigulla <nagasitaram.ram@gmail.com>
2018-11-30 20:04:26 +0530

    commit 20:04:26.26,Fri 11/30/2018

--- bot/models.py
+++ bot/models.py
@@ -1,4 +1,4 @@
-from datetime import timedelta,datetime
+from datetime import timedelta, datetime
 
 from django.db import models
 
@@ -38,5 +38,5 @@ class CheckinStore(models.Model):
         if self.check_out_time == None:
             self.check_out_time = pytz.timezone("Asia/Kolkata").localize(datetime.now())
         time_diff: timedelta = self.check_out_time - self.check_in_time
-        self.total_time = round(time_diff.total_seconds() / 3600,
+        self.total_time = round(time_diff.total_seconds() / 3600, 3)
         super(CheckinStore, self).save(*args, **kwargs)
