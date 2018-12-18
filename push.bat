131c92e0e3571119fd3eb4306e2edf071fa3a2c4
SatyaSarika <sunkara.satyasarika@gmail.com>
2018-12-18 09:50:29 +0530

    commit  9:50:29.68,Tue 12/18/2018

--- push.bat
+++ push.bat
@@ -2,7 +2,5 @@
 git init .
 git add .
 git commit -m  "commit %time%,%date%"
-git rm  --cached SaanviDashboard -r
-git rm --cached .idea -r
 git remote add -m origin qnaforum https://github.com/nagasitaramthigulla/sanvi-bot-django.git
 git push -u origin master
